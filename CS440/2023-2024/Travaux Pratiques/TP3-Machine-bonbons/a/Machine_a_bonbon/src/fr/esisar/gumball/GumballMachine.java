0f9c19832887b25f86861e5963947a0d16fb87c0
Lysio4 <lukas.moncorge@gmail.com>
2023-11-29 12:08:23 +0100

    plus que la q5 et la q6

--- CS440/2023-2024/Travaux Pratiques/TP3-Machine-bonbons/a/Machine_a_bonbon/src/fr/esisar/gumball/GumballMachine.java
+++ CS440/2023-2024/Travaux Pratiques/TP3-Machine-bonbons/a/Machine_a_bonbon/src/fr/esisar/gumball/GumballMachine.java
@@ -3,19 +3,33 @@ package fr.esisar.gumball;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Cette classe représente l'implémentation d'une classe Gumball Machine. Ici,
+ * on n'implémente que la machine en elle-même.
+ * 
+ * @author Leprat-Moncorgé
+ * @version 2.0
+ */
 public class GumballMachine {
 
 	private static final Logger LOGGER = LogManager.getLogger(GumballMachine.class);
 
-	static final int SOLD_OUT = 0;
-	static final int NO_QUARTER = 1;
-	static final int HAS_QUARTER = 2;
-	static final int SOLD = 3;
-
+	/**
+	 * Le nombre de bonbons dans la machine, initialisé par défaut à 0.
+	 */
 	private int count = 0;
 
+	/**
+	 * L'état initial de la machine, par défaut à Sold Out.
+	 */
 	private GumballMachineState state = new GumballSoldOut();
 
+	/**
+	 * Constructeur pour la machine, qui permet d'initialiser le nombre de bonbons
+	 * et l'état.
+	 *
+	 * @param Le nombre de bonbons dans la machine
+	 */
 	public GumballMachine(int count) {
 		super();
 		this.count = count;
@@ -24,43 +38,89 @@ public class GumballMachine {
 		}
 	}
 
-	public static Logger getLogger() {
-		return LOGGER;
-	}
-
-	public int getCount() {
-		return count;
-	}
-
-	public void setCount(int count) {
-		this.count = count;
-	}
-
-	public GumballMachineState getState() {
-		return state;
-	}
-
-	public void setState(GumballMachineState state) {
-		this.state = state;
-	}
-
+	/**
+	 * Insère une pièce dans la machine.
+	 *
+	 */
 	public void insertQuarter() {
 		state.insertQuarter(this);
 	}
 
+	/**
+	 * Éjecte une pièce de la machine.
+	 *
+	 */
 	public void ejectQuarter() {
 		state.ejectQuarter(this);
 	}
 
+	/**
+	 * Tourne la manivelle.
+	 *
+	 */
 	public void turnCrank() {
 		state.turnCrank(this);
 	}
 
+	/**
+	 * Distribue un bonbon.
+	 *
+	 */
 	@SuppressWarnings("unused")
 	private void dispense() {
 		state.dispenseState(this);
 	}
 
+	/**
+	 * Getter pour le logger.
+	 *
+	 * @return LOGGER
+	 */
+	public static Logger getLogger() {
+		return LOGGER;
+	}
+
+	/**
+	 * Getter pour le nombre de bonbons dans la machine.
+	 *
+	 * @return count
+	 */
+	public int getCount() {
+		return count;
+	}
+
+	/**
+	 * Setter pour le nombre de bonbons dans la machine.
+	 *
+	 * @param nouveau count
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	/**
+	 * Getter pour l'état.
+	 *
+	 * @return state
+	 */
+	public GumballMachineState getState() {
+		return state;
+	}
+
+	/**
+	 * Setter pour l'état de la machine.
+	 *
+	 * @param nouveau state
+	 */
+	public void setState(GumballMachineState state) {
+		this.state = state;
+	}
+	
+	/**
+	 * Méthode toString de la machine.
+	 *
+	 * @return String
+	 */
 	@Override
 	public String toString() {
 		StringBuilder result = new StringBuilder();
